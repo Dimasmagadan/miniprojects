d3c51bfc0b1a7305c3b671b370b99513bae0f71e
Chris Knowles <knowles.chris.d@gmail.com>
2014-01-23 17:17:30 +1100

    General updates

--- supercharged-post-nav/supercharged_post_previews.php
+++ supercharged-post-nav/supercharged_post_previews.php
@@ -1,47 +1,55 @@
 <?php
+/**
+ * Plugin Name: Supercharged Post Navigation
+ * Plugin URI: http://premium.wpmudev.org/blog
+ * Description: Provides a much enhanced post navigation via supercharged_post_navigation function
+ * Version: The Plugin's Version Number, e.g.: 1.0
+ * Author: Chris Knowles
+ * Author URI: http://premium.wpmudev.org/blog
+ * License: A "Slug" license name e.g. GPL2
+ */
 
-// turn into a plugin
-
-function post_preview() {
+function supercharged_post_navigation( $title_text='> next post' , $image_size='post_thumbnail' ) {
 					
-	$next_post = get_previous_post();
+	$in_same_term = true; // get posts from same category
+	$excluded_terms = ''; // included all categories
+	$previous = true; // get previous post - for date descending next post is previous				
+	
+	// get the "previous" post in same category				
+	$next_post = get_adjacent_post( $in_same_term, $excluded_terms, $previous);
+	
+	// nothing found? try without category
+	if ( !$next_post ) {
+	
+		$in_same_term = false; // get post regardless of category
+		$next_post = get_adjacent_post( $in_same_term, $excluded_terms, $previous);
+	}
 					
+	//got a post!				
 	if ($next_post) {
-					
+		
+		// get the featured image			
 		$thumb_id = get_post_thumbnail_id($next_post->ID);
-		$thumb_url = wp_get_attachment_image_src($thumb_id,'large', true);
-					
+		$thumb_url = wp_get_attachment_image_src($thumb_id, $image_size , true);
+		
+		// get the excerpt - IMPORTANTL: You must use either the excerpt metabox or the <!--more--> tag		
 		if ( has_excerpt( $next_post->ID ) ) {
 			$the_excerpt = $next_post->post_excerpt;
 		} else {
 			$the_excerpt = substr( $next_post->post_content, 0, strpos( $next_post->post_content, '<!--more-->' ) );
 		}
 		
+		echo '
+		<div class="next-post-preview" style="padding: 30px; opacity:0.7; background-image: url(' . $thumb_url[0] . ')">
+			<a href="' . get_permalink( $next_post->ID ) . '">
+				<div class="next-post-preview-content" style="width: 80%; margin: auto; background-color: #000; opacity: 0.7; padding: 20px; ">					
+					<div style="color: #fff; width: 100%; border-top: 2px solid #fff; margin-top: 20px; padding-top: 5px; text-transform: uppercase">' . $title_text . '</div>				
+					<h2 style="color: #fff;">' . $next_post->post_title . '</h2>
+					<div style="color: #fff; font-weight: bold;">' . apply_filters( 'the_content' , $the_excerpt ) . '</div> 
+				</div>
+			</a>	
+		</div>';
 		
-	// leave HTML in here
-	// move CSS to own file or leave as is?	
-						 
-?>
-					
-	<div class="next-post-preview" style="padding: 30px; opacity:0.7; background-image: url(<?php echo $thumb_url[0]; ?>)">
-					
-		<a href="<?php echo get_permalink( $next_post->ID ) ?>">
-					
-			<div class="next-post-preview-content" style="width: 80%; margin: auto; background-color: #000; opacity: 0.8; padding: 20px; ">
-						
-			<div style="color: #fff; width: 100%; border-top: 2px solid #fff; margin-top: 20px; padding-top: 5px; text-transform: uppercase">> next post </div>
-											
-				<h2 style="color: #fff;"><?php echo $next_post->post_title; ?></h2>
-						
-				<div style="color: #fff; font-weight: bold;"><?php echo apply_filters( 'the_content', $the_excerpt ); ?></div>
-						
-			</div>
-						
-		</a>
-					
-	</div>
+	}
 	
-<?php
-
-}
-
+} // end function
